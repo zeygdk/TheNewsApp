bd83a7f33f479f063769c6659a78248df86dec99
Zeynep <zeynepgedik.gdk@gmail.com>
2024-01-25 14:32:20 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/dailycheckapp/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.example.dailycheckapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application()
---
+++ app/src/main/java/com/example/dailycheckapp/onboarding/navigation/NavGraph.kt
@@ -0,0 +1,31 @@
+package com.example.dailycheckapp.onboarding.navigation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.dailycheckapp.onboarding.screen.HomeScreen
+import com.example.dailycheckapp.onboarding.screen.WelcomeScreen
+
+import com.google.accompanist.pager.ExperimentalPagerApi
+
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@Composable
+fun SetupNavGraph(
+    navController: NavHostController,
+    startDestination: String
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(route = Screen.Welcome.route) {
+            WelcomeScreen(navController = navController)
+        }
+        composable(route = Screen.Home.route) {
+            HomeScreen()
+        }
+    }
+}
